ebd89aac81097ba93544bc2e4b0766577f0f1a24
ProgrammerNelitha <54647556+ProgrammerNelitha@users.noreply.github.com>
2023-04-15 19:16:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -47,6 +47,39 @@ def findSelectedCell(app,mx,my):
                 return (row,col)
     return (None,None)
 #-------------------------------------------------------------------------------
+def getSquareRegion(board,row,col):
+    if row%3==0: rowsList=[row,row+1,row+2]
+    elif row%3==1: rowsList=[row-1,row,row+1]
+    elif row%3==2: rowsList=[row-2,row-1,row]
+
+    if col%3==0: colsList=[col,col+1,col+2]
+    elif col%3==1: colsList=[col-1,col,col+1]
+    elif col%3==2: colsList=[col-2,col-1,col]
+
+    vals=[]
+    for num1 in rowsList:
+        for num2 in colsList:
+            if board[num1][num2]!=0:
+                vals.append(board[num1][num2])
+    return vals
+
+def getLegals(board,row,col):
+    rows,cols=len(board),len(board[0])
+    legals=set()
+    colList=[]
+    for rowNum in range(rows):
+        colList.append(board[rowNum][col])
+    square=getSquareRegion(board,row,col)
+
+    for num in range(1,10):
+        #Check Row
+        if num not in board[row]:
+            if num not in colList:
+                #Check Square
+                if num not in square:
+                    legals.add(num)
+    return legals
+#-------------------------------------------------------------------------------
 #Backtracking code modified from the Mini-Sudoku solver
 '''
 #Backtraking
@@ -109,6 +142,23 @@ def isLegalSudoku(board):
 '''
 #-------------------------------------------------------------------------------
 #Board loading from Sudoku hints
+def getBoard(difficulty):
+    path = random.choice(loadBoardPaths(difficulty))
+    file = readFile(f"C:\\Users\\1234l\\Documents\\Documents\\CMU\\15112\\term project\\tp-starter-files\\tp-starter-files\\{path}")
+    file = file.splitlines()
+    board=[]
+    for line in file:
+        rowList=[]
+        for num in line.split(' '):
+            rowList.append(int(num))
+        board.append(rowList)
+    return board
+
+
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
 def loadBoardPaths(filters):
         boardPaths = [ ]
         for filename in os.listdir(f'C:\\Users\\1234l\\Documents\\Documents\\CMU\\15112\\term project\\tp-starter-files\\tp-starter-files\\boards'):
@@ -126,13 +176,9 @@ def hasFilters(filename, filters=None):
 
 #-------------------------------------------------------------------------------
 #Splash Screen
-def onAppStart(app):
-    app.board=None
-
 def splash_onKeyPress(app, key):
     if key == 'h': setActiveScreen('help')
     elif key=='enter': 
-        app.board = random.choice(loadBoardPaths('easy'))
         setActiveScreen('game')
 
 def splash_onMousePress(app,mouseX,mouseY):
@@ -156,14 +202,28 @@ def game_onAppStart(app):
     #Initiailze board
     app.rows = 9
     app.cols = 9
-    #app.board = [([None] * app.cols) for row in range(app.rows)]
+    
+    app.board=getBoard('easy')
+    app.banned=[]
+    app.legals=[]
+    for row in range(app.rows):
+        for col in range(app.cols):
+            if app.board[row][col]!=0: 
+                app.banned.append((row,col))
+            else:
+                legalSet=getLegals(app.board,row,col)
+                app.legals.append((row,col,legalSet))
+
     app.boardLeft = 75
     app.boardTop = 130
     app.boardWidth = 550
     app.boardHeight = 550
     app.cellBorderWidth = 2
+    
     app.selectedCellX=None
     app.selectedCellY=None
+
+    app.showLegals=False
     # app.solvedBoard=sudokuSolver(app)
 
 def game_onMousePress(app,mouseX,mouseY):
@@ -173,7 +233,8 @@ def game_onMousePress(app,mouseX,mouseY):
 def game_onKeyPress(app,key):
     if app.selectedCellX!=None:
         if key.isdigit():
-            app.board[app.selectedCellX][app.selectedCellY]=int(key)
+            if (app.selectedCellX,app.selectedCellY) not in app.banned:
+                app.board[app.selectedCellX][app.selectedCellY]=int(key)
 
 def game_redrawAll(app):
     drawRect(0,0,app.width,app.height,fill='yellow',opacity=15)
@@ -183,9 +244,21 @@ def game_redrawAll(app):
         drawCell(app, app.selectedCellX, app.selectedCellY, None, 'red', app.cellBorderWidth*3)
     for row in range(app.rows):
         for col in range(app.cols):
-            if app.board[row][col]!=None:
+            if app.board[row][col]!=0:
                 numX, numY = getCellLeftTop(app, row,col)
                 w, h = getCellSize(app)
                 drawLabel(app.board[row][col],numX+w/2,numY+h/2,size=25)
+                if (row,col) in app.banned:
+                    drawRect(numX,numY,w,h,fill=rgb(135,62,133),opacity=25)
+                else:
+                    for legalVal in getLegals(app.board,row,col):
+                        drawLegal(legalVal,row,col)
+
+def drawLegal(legalVal,row,col):
+    numX, numY = getCellLeftTop(app, row,col)
+    if int(legalVal)==1:
+        x=numX+5
+        y=numY+5
+    drawLabel(legalVal,x,y,size=10)
 #-------------------------------------------------------------------------------
 runAppWithScreens(initialScreen='splash',width=1000,height=700)
